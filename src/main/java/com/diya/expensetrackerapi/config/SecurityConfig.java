b2bbda2b302d8502f0d2f50a68573dea92e4c9fe
alshoubakidiya <alshoubakidiya@gmail.com>
2026-08-08 14:55:46 +0300

    URL fixes

--- src/main/java/com/diya/expensetrackerapi/config/SecurityConfig.java
+++ src/main/java/com/diya/expensetrackerapi/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
